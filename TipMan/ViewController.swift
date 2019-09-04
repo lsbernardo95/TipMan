9e4baa392fd40cb4bd2baf727264c8c870010f3c
Laurence Bernardo <lsbernardo95@gmail.com>
2019-09-04 16:19:34 -0700

    First commit

--- TipMan/ViewController.swift
+++ TipMan/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipAmount: UILabel!
+    @IBOutlet weak var totalAmount: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var GifView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func onTap(_ sender: Any) {
+        print("Hello")
+        view.endEditing(true)
+    }
+    @IBAction func CalculateTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        let tipPercentages = [0.1, 0.15, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipAmount.text = String(format: "$%.2f", tip)
+        totalAmount.text = String(format: "$%.2f", total)
+        if (tipPercentages[tipControl.selectedSegmentIndex] > 0.15){
+            GifView.loadGif(name: "crying")
+        }
+        else if (tipPercentages[tipControl.selectedSegmentIndex] > 0.1){
+            GifView.loadGif(name: "thankful")
+        }
+        else{
+            GifView.loadGif(name: "mad")
+        }
+    }
 
 }
 
